1cf28148d62cc84fe93dc12f15600a96d7a35f84
liang-zhengxin <85280491+liang-zhengxin@users.noreply.github.com>
2021-07-01 19:48:19 +0800

    added checking script for missing data

---
+++ scripts/check.py
@@ -0,0 +1,30 @@
+"""
+Missing Data Checker
+
+This script checks for missing dates between a given timeframe. It does not check if the data is accurate.
+"""
+# Import Required Modules
+import json
+import datetime
+
+# Global Variables 
+START_DATE = datetime.datetime(2020,10,1)
+END_DATE = datetime.datetime(2021,6,1)
+
+# Load Data Function
+def load():
+    with open("SG-COVIDdata/data/pre1jun21.json") as file:
+        dailyLocalCases = json.load(file)
+    return dailyLocalCases
+
+data = load() # Initialisation
+
+# Main Function
+while True:
+    if START_DATE == END_DATE:
+        break
+
+    START_DATE_STR = START_DATE.strftime("%d-%m-%Y")
+    if START_DATE_STR not in data:
+        print(START_DATE_STR)
+    START_DATE += datetime.timedelta(days=1)
