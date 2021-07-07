cb1611d9a0f10c618818e7b70990e4b159e3d78e
liang-zhengxin <85280491+liang-zhengxin@users.noreply.github.com>
2021-07-07 20:15:54 +0800

    updated website

--- website/src/main.js
+++ website/src/main.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('value');
 const progressValue = document.querySelector('.Progressbar__value');
 const percent = document.querySelector('.percent');
+const date = document.querySelector('.date');
 
 function setValue(value) {
   progressValue.style.width = `${value}%`;
@@ -20,16 +21,17 @@ const latestData = {
   }
 };
 
-// fetch("../api/latest.json")
-//  .then(response => response.json())
-//  .then(data=> {getValue(data)});
-getValue(latestData)
+fetch("liang-zhengxin.github.io/SG-COVIDdata/api/latest.json")
+  .then(response => response.json())
+  .then(data=> {getValue(data)});
+
+// getValue(latestData)
 function getValue(latestData) {
   const pop = 5685800
   const t = Object.keys(latestData.dailyVaxData)
   var completed = latestData.dailyVaxData[t].completed
+  date.textContent = "Updated: " + t
   var percent =(completed/pop)*100
   setValue(percent)
 }
-
 
