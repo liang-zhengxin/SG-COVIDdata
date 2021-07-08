c79b3622d5c5eed407d0741b97cf8a40654d92e9
liang-zhengxin <85280491+liang-zhengxin@users.noreply.github.com>
2021-07-08 15:33:41 +0800

    added milestones

--- website/src/main.js
+++ website/src/main.js
@@ -1,4 +1,3 @@
-const input = document.getElementById('value');
 const progressValue = document.querySelector('.Progressbar__value');
 const percent = document.querySelector('.percent');
 const date = document.querySelector('.date');
@@ -25,7 +24,7 @@ fetch("api/latest.json")
   .then(response => response.json())
   .then(data=> {getValue(data)});
 
-// getValue(latestData)
+getValue(latestData)
 function getValue(latestData) {
   const pop = 5685800
   const t = Object.keys(latestData.dailyVaxData)
@@ -33,5 +32,19 @@ function getValue(latestData) {
   date.textContent = "Updated: " + t
   var percent =(completed/pop)*100
   setValue(percent)
+  getMilestone(50-percent)
 }
 
+// Adding Text Milestones
+function getMilestone(percentageLeft) {
+  const milestone = document.createElement("h4");
+  if (percentageLeft <= 0) {
+    //  block of code to be executed if the condition is true
+    document.getElementById("target").innerHTML = "50% Vaccination Milestone Achieved";
+  } else {
+    var figure = document.createTextNode(String(Math.round(percentageLeft)) + "% remaining to achieve target");
+    milestone.appendChild(figure);
+    const element = document.querySelector('.Texttargets');
+    element.appendChild(milestone);
+  }
+}
